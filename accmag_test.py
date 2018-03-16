b61dc0630d4706c36592b101c9428eee67deb390
jacksder <jacksder@oregonstate.edu>
2018-03-16 12:20:45 -0700

    third comm

--- accmag_test.py
+++ accmag_test.py
@@ -12,14 +12,24 @@ import Adafruit_LSM303
 def compass(x,y,z):
 	bear = 0
 	pi = 3.14159265359
+	
+	alpha = math.atan(x)
+	gamma = math.atan(y)
+	print('roll: {0}  pitch: {1}'.format(math.degrees(gamma),math.degrees(alpha)))
+
+	x_h = x*math.cos(alpha) + y*math.sin(gamma)*math.sin(alpha)
+			- z*math.cos(gamma)*math.sin(alpha)
+	y_h = y*math.cos(gamma) - z*math.sin(gamma)
+	
+
 	if (y>0):
-		bear = 90 - (math.atan(x/y)*180/pi)
-	else if (y<0):
-		bear = 270 - (math.atan(x/y)*180/pi)
-	else if (x < 0):
-		bear = 180
+		bear = 15 + 90 - (math.atan(x_h/y_h)*180/pi)
+	elif (y<0):
+		bear = 15 + 270 - (math.atan(x_h/y_h)*180/pi)
+	elif (x < 0):
+		bear = 180 + 15
 	else:
-		bear = 0
+		bear = 0 + 15
 	print ('North and {0}'.format(bear))
 
 # Create a LSM303 instance.
@@ -37,8 +47,8 @@ while True:
     mag_x, mag_z, mag_y = mag
 
 
-    #print('Accel X={0}, Accel Y={1}, Accel Z={2}, Mag X={3}, Mag Y={4}, Mag Z={5}'.format(
-          #accel_x, accel_y, accel_z, mag_x, mag_y, mag_z))
+    print('Accel X={0}, Accel Y={1}, Accel Z={2}, Mag X={3}, Mag Y={4}, Mag Z={5}'.format(
+    	accel_x, accel_y, accel_z, mag_x, mag_y, mag_z))
 	compass(mag_x,mag_y,mag_z)
     # Wait half a second and repeat.
-    time.sleep(0.5)
+    time.sleep(2)
